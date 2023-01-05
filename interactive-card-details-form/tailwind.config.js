fac226e7bfd89cf224032a5896220ab20d9c0bd6
mahmoudtarek <mahmoudtarek@code.bld.ai>
2023-01-05 14:59:04 +0200

    finshing mobile design

--- interactive-card-details-form/tailwind.config.js
+++ interactive-card-details-form/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: {
+          max: "1024px",
+        },
+        "mobile-sm": {
+          max: "375px",
+        },
+      },
+    },
   },
   plugins: [],
+  important: true,
 };
