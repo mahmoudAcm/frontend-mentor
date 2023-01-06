6b06530c925f0180657fedae6cd3569413840156
mahmoudtarek <mahmoudtarek@code.bld.ai>
2023-01-06 14:27:56 +0200

    adding source code links

--- data.js
+++ data.js
@@ -1,14 +1,22 @@
 const solutions = [
   {
     src: "./launch-countdown-timer/design/screenshot.png",
-    url: "https://mahmoudacm.github.io/frontend-mentor/launch-countdown-timer",
+    livePreviewLink:
+      "https://mahmoudacm.github.io/frontend-mentor/launch-countdown-timer",
+    sourceCodeLink: "https://github.com/mahmoudAcm/frontend-mentor/tree/master/launch-countdown-timer",
   },
   {
     src: "./interactive-comments-section/design/screenshot.png",
-    url: "https://mahmoudacm.github.io/frontend-mentor/interactive-comments-section/dist/",
+    livePreviewLink:
+      "https://mahmoudacm.github.io/frontend-mentor/interactive-comments-section/dist/",
+    sourceCodeLink:
+      "https://github.com/mahmoudAcm/frontend-mentor/tree/master/interactive-comments-section",
   },
   {
     src: "./interactive-card-details-form/design/screenshot.png",
-    url: "https://mahmoudacm.github.io/frontend-mentor/interactive-card-details-form/",
+    livePreviewLink:
+      "https://mahmoudacm.github.io/frontend-mentor/interactive-card-details-form/",
+    sourceCodeLink:
+      "https://github.com/mahmoudAcm/frontend-mentor/tree/master/interactive-card-details-form",
   },
 ];
