96f142b1d1da180a07e1131cdceaf38ff3a41181
mahmoudtarek <mahmoudtarek@code.bld.ai>
2023-01-09 00:04:13 +0200

    building the home page

--- data.[id].js
+++ data.[id].js
@@ -3,7 +3,8 @@ const solutions = [
     src: "https://mahmoudacm.github.io/frontend-mentor/launch-countdown-timer/design/screenshot.png",
     livePreviewLink:
       "https://mahmoudacm.github.io/frontend-mentor/launch-countdown-timer",
-    sourceCodeLink: "https://github.com/mahmoudAcm/frontend-mentor/tree/master/launch-countdown-timer",
+    sourceCodeLink:
+      "https://github.com/mahmoudAcm/frontend-mentor/tree/master/launch-countdown-timer",
   },
   {
     src: "https://mahmoudacm.github.io/frontend-mentor/interactive-comments-section/design/screenshot.png",
@@ -18,5 +19,12 @@ const solutions = [
       "https://mahmoudacm.github.io/frontend-mentor/interactive-card-details-form/",
     sourceCodeLink:
       "https://github.com/mahmoudAcm/frontend-mentor/tree/master/interactive-card-details-form",
+  },
+  {
+    src: "https://mahmoudacm.github.io/frontend-mentor/rock-paper-scissors/design/screenshot.png",
+    livePreviewLink:
+      "https://mahmoudacm.github.io/frontend-mentor/rock-paper-scissors/dist",
+    sourceCodeLink:
+      "https://github.com/mahmoudAcm/frontend-mentor/tree/master/rock-paper-scissors",
   },
 ];
