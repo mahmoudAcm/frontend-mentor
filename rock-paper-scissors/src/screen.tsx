f4a24f665f32f55f4b50047927f428d394692139
mahmoudtarek <mahmoudtarek@code.bld.ai>
2023-01-08 23:53:51 +0200

    building the project

--- rock-paper-scissors/src/screen.tsx
+++ rock-paper-scissors/src/screen.tsx
@@ -7,7 +7,7 @@ export default function Screen() {
   const { score } = useGame();
   return (
     <StyledScreen>
-      <img src="/images/logo.svg" alt="logo" />
+      <img src="./images/logo.svg" alt="logo" />
       <Box className="score">
         <span className="text">SCORE</span>
         <span className="value">{score}</span>
