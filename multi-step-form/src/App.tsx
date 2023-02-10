0a3dfdf8a46b8ae26f625728ca731cfa1875ccfe
mahmoudtarek <mahmoudtarek@code.bld.ai>
2023-02-10 15:43:47 +0200

    adding icons

--- multi-step-form/src/App.tsx
+++ multi-step-form/src/App.tsx
@@ -1,3 +1,15 @@
+import ThankYouIcon from "./icons/ThankYou";
+import ArcadeIcon from "./icons/Arcade";
+import ProIcon from "./icons/Pro";
+import CheckMarkIcon from "./icons/CheckMark";
+
 export default function App() {
-  return <></>
+  return (
+    <>
+      <ThankYouIcon fontSize="large" />
+      <ArcadeIcon fontSize="large" />
+      <ProIcon fontSize="large" />
+      <CheckMarkIcon fontSize="large" />
+    </>
+  );
 }
---
+++ multi-step-form/src/icons/CheckMark.tsx
@@ -0,0 +1,15 @@
+import { createSvgIcon } from "@mui/material";
+
+const CheckMark = createSvgIcon(
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 9">
+    <path
+      fill="none"
+      stroke="#FFF"
+      stroke-width="2"
+      d="m1 4 3.433 3.433L10.866 1"
+    />
+  </svg>,
+  "CheckMark"
+);
+
+export default CheckMark;
