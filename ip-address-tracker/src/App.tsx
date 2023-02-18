3f6cb0321d516115be60a6a72ed16bc49e62cde1
mahmoudtarek <mahmoudtarek@code.bld.ai>
2023-02-18 21:59:48 +0200

    initial commit

---
+++ ip-address-tracker/src/App.tsx
@@ -0,0 +1,11 @@
+import { CssBaseline } from "@mui/material";
+import Comp from "./comp";
+
+export default function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <Comp />
+    </div>
+  );
+}
