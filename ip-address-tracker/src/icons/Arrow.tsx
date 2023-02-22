2dd93b2043370bf731d94712e47c5b7c2a186604
mahmoudtarek <mahmoudtarek@code.bld.ai>
2023-02-22 12:08:56 +0200

    fix: importing the correct props type for SVG

--- ip-address-tracker/src/icons/Arrow.tsx
+++ ip-address-tracker/src/icons/Arrow.tsx
@@ -1,11 +1,11 @@
-import { createSvgIcon, SvgProps } from "@mui/material";
+import { createSvgIcon, SvgIconProps } from "@mui/material";
 
 const Arrow = createSvgIcon(
   <path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" />,
   "Arrow"
 );
 
-export default function ArrowIcon(props: SvgProps) {
+export default function ArrowIcon(props: SvgIconProps) {
   return (
     <Arrow viewBox="0 0 11 14" sx={{ width: 11, height: 14 }} {...props} />
   );
