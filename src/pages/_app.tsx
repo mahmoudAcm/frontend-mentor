fecdb7291821ca87b6897630fb05818c7ee9c5d5
mahmoudAcm <mahmoud.acm.123@gmail.com>
2023-04-08 05:28:34 +0200

    Added base theme and dark theme palettes.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/src/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/src/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from '../../theme';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {loading ? <></> : <Component {...pageProps} />}
+    </ThemeProvider>
+  );
 }
