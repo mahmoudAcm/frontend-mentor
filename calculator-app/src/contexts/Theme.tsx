3fb4fc2fe912d9229c859024ce7d53a345b4014c
mahmoudAcm <mahmoud.acm.123@gmail.com>
2023-04-23 14:56:15 +0200

    Add functionality to store theme data in localStorage.

--- calculator-app/src/contexts/Theme.tsx
+++ calculator-app/src/contexts/Theme.tsx
@@ -1,4 +1,4 @@
-import { createContext, Dispatch, ReactNode, useState } from 'react';
+import { createContext, Dispatch, ReactNode, useEffect, useState } from 'react';
 import { ThemeProvider as DefaultThemeProvider } from '@mui/material';
 import createCustomTheme from '../theme';
 import { THEMES } from '../constants';
@@ -9,11 +9,32 @@ export const ThemeContext = createContext({
 });
 
 export default function ThemeProvider({ children }: { children: ReactNode }) {
-  const [theme, setTheme] = useState(THEMES.THEME2);
+  const [theme, toggleTheme] = useState(THEMES.THEME2);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      toggleTheme(theme);
+    }
+    setLoading(false);
+  }, []);
+
+  if (loading) return;
 
   return (
     <DefaultThemeProvider theme={createCustomTheme({ theme })}>
-      <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>
+      <ThemeContext.Provider
+        value={{
+          theme,
+          setTheme: theme => {
+            localStorage.setItem('theme', theme);
+            toggleTheme(theme);
+          }
+        }}
+      >
+        {children}
+      </ThemeContext.Provider>
     </DefaultThemeProvider>
   );
 }
