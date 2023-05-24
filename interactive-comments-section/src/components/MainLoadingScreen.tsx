1c4698c3bc409d14faf3b0cdae2979274682e9f3
mahmoudAcm <mahmoud.acm.123@gmail.com>
2023-05-24 15:05:39 +0300

    [Refactor] Updated main loading JSON file and styles

--- interactive-comments-section/src/components/MainLoadingScreen.tsx
+++ interactive-comments-section/src/components/MainLoadingScreen.tsx
@@ -1,7 +1,7 @@
 import Lottie from 'lottie-react';
 import loader from '@/src/components/loader.json';
 import { Backdrop } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface MainLoadingScreenProps {
   loading: boolean;
@@ -10,25 +10,32 @@ interface MainLoadingScreenProps {
 export default function MainLoadingScreen(props: MainLoadingScreenProps) {
   const [isLooping, setLooping] = useState(true);
 
-  return <></>;
+  useEffect(() => {
+    if (!isLooping) {
+      document.body.style.overflow = 'auto';
+    } else document.body.style.overflow = 'hidden';
+  }, [isLooping]);
 
   return (
-    <Backdrop
-      open={isLooping}
-      appear={false}
-      sx={{
-        background: theme => theme.palette.background.default,
-        zIndex: theme => theme.zIndex.appBar * 2,
-        minHeight: '100%'
-      }}
-    >
-      <Lottie
-        animationData={loader}
-        loop={isLooping}
-        onLoopComplete={() => {
-          if (!props.loading) setLooping(false);
+    <>
+      <Backdrop
+        open={isLooping}
+        appear={false}
+        sx={{
+          background: theme => theme.palette.background.default,
+          zIndex: theme => theme.zIndex.appBar * 2,
+          minHeight: '100%'
         }}
-      />
-    </Backdrop>
+      >
+        <Lottie
+          animationData={loader}
+          loop={isLooping}
+          style={{ position: 'absolute', inset: 0, margin: 'auto', maxWidth: '50%' }}
+          onLoopComplete={() => {
+            if (!props.loading) setLooping(false);
+          }}
+        />
+      </Backdrop>
+    </>
   );
 }
