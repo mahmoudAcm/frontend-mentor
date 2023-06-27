ecd676adb4c4ef4d936509c4f00f628acfcfcb47
mahmoudAcm <mahmoud.acm.123@gmail.com>
2023-06-27 23:09:56 +0300

    Fix: Used Mui popover to ensure the mention card does not overflow the page when displayed.

--- interactive-comments-section/src/components/Comment/Mention.tsx
+++ interactive-comments-section/src/components/Comment/Mention.tsx
@@ -1,10 +1,22 @@
 import useCommentOrReplyContext from '@/src/hooks/useCommentOrReplyContext';
-import { Box, Paper, Typography } from '@mui/material';
+import { Box, Paper, Popover, Typography } from '@mui/material';
 import Avatar from '@/src/components/MuiAvatar';
+import { MouseEvent, useState } from 'react';
 
 export default function Mention({ children }: { children: string }) {
   const { mentions } = useCommentOrReplyContext();
   const userData = mentions.find(mention => mention.user.username === children.slice(1))?.user!;
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
+  const open = Boolean(anchorEl);
+
+  const handlePopoverOpen = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
 
   if (!userData) return <>{children}</>;
 
@@ -15,47 +27,57 @@ export default function Mention({ children }: { children: string }) {
       aria-label={`Mentioned user: ${children}`}
       sx={{
         cursor: 'pointer',
-        position: 'relative',
-        display: 'inline-block',
-        '&:hover .user': {
-          zIndex: 2,
-          opacity: 1
-        }
+        display: 'inline-block'
       }}
+      aria-owns={open ? 'mouse-over-popover' : undefined}
+      aria-haspopup='true'
+      onMouseEnter={handlePopoverOpen}
+      onMouseLeave={handlePopoverClose}
     >
       {children}
-      <Paper
+      <Popover
+        id='mouse-over-popover'
         sx={{
-          position: 'absolute',
-          bottom: 30,
-          p: 1.3,
-          pr: 4,
-          display: 'flex',
-          width: 'max-content',
-          alignItems: 'center',
-          zIndex: -1,
-          opacity: 0,
-          transition: theme =>
-            theme.transitions.create('opacity', {
-              duration: 200
-            })
+          pointerEvents: 'none'
         }}
-        className='user'
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left'
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+        keepMounted
       >
-        <Avatar src={userData.image} alt='user details' />
-        <Box sx={{ ml: 1.3 }}>
-          <Typography variant='h2'>{userData.username}</Typography>
-          <Typography
-            variant='subtitle2'
-            sx={{
-              fontWeight: '400'
-            }}
-            color='textSecondary'
-          >
-            {userData.email}
-          </Typography>
-        </Box>
-      </Paper>
+        <Paper
+          sx={{
+            p: 1.3,
+            pr: 4,
+            display: 'flex',
+            minWidth: '100px',
+            alignItems: 'center'
+          }}
+        >
+          <Avatar src={userData.image} alt='user details' />
+          <Box sx={{ ml: 1.3 }}>
+            <Typography variant='h2'>{userData.username}</Typography>
+            <Typography
+              variant='subtitle2'
+              sx={{
+                fontWeight: '400'
+              }}
+              color='textSecondary'
+            >
+              {userData.email}
+            </Typography>
+          </Box>
+        </Paper>
+      </Popover>
     </Typography>
   );
 }
