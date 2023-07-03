fe9394a992cf5bc256128f0d3ac7f8fa88142584
mahmoudAcm <mahmoud.acm.123@gmail.com>
2023-07-03 18:37:00 +0300

    Fix: Show loading skeleton only if the images are not loaded.

--- interactive-comments-section/src/components/MuiAvatar.tsx
+++ interactive-comments-section/src/components/MuiAvatar.tsx
@@ -1,17 +1,22 @@
 import AvatarDefault, { AvatarProps } from '@mui/material/Avatar';
-import { useState } from 'react';
-import { Box, BoxProps, Fade, Skeleton, useTheme } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
+import { Box, BoxProps, Fade, Skeleton } from '@mui/material';
 
-interface MuiAvatarProps extends AvatarProps {
+interface MuiAvatarProps extends Omit<AvatarProps, 'ref'> {
   overlaySx?: BoxProps['sx'];
 }
 
 export default function MuiAvatar(props: MuiAvatarProps) {
-  const theme = useTheme();
   const { onLoad, onLoadStart, overlaySx, ...rest } = props;
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const imageRef = useRef<HTMLDivElement | null>(null);
 
   const overlayStyles = { position: 'absolute', inset: 0, width: '100%', height: '100%' };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    if (imageRef.current && !(imageRef.current?.querySelector('img')! as HTMLImageElement).complete) setLoading(true);
+  });
 
   return (
     <Box
@@ -27,19 +32,12 @@ export default function MuiAvatar(props: MuiAvatarProps) {
     >
       <AvatarDefault
         {...rest}
+        ref={imageRef}
         onLoad={() => {
           setLoading(false);
         }}
       />
-      <Fade
-        in={loading}
-        appear={false}
-        timeout={!loading ? 700 : 0}
-        easing={{
-          exit: theme.transitions.easing.easeOut,
-          enter: theme.transitions.easing.sharp
-        }}
-      >
+      <Fade in={loading}>
         <Box sx={{ ...overlayStyles, zIndex: 1, background: 'white', borderRadius: '50%', overflow: 'hidden' }}>
           <Skeleton variant='rectangular' animation='wave' sx={{ ...overlayStyles, zIndex: 2 }} />
         </Box>
